0cb7551db5fe73189ced431bf694ce38b5eaf387
Nesh108 <albertovaccari108@gmail.com>
2015-04-06 03:09:34 +0200

    Exercise 1

--- introduction/set_joint_commands.py
+++ introduction/set_joint_commands.py
@@ -16,6 +16,20 @@ class MyAgent(SparkAgent):
     def think(self, perception):
         action = super(MyAgent, self).think(perception)
         # YOUR CODE HERE
+        action.stiffness["LShoulderPitch"] = 0
+        action.stiffness["RShoulderPitch"] = 0
+
+        action.speed["HeadYaw"] = 0.1
+        action.speed["RShoulderRoll"] = -0.1
+        action.speed["LShoulderRoll"] = 0.1
+        action.speed["LElbowRoll"] = -0.1
+        action.speed["RElbowRoll"] = 0.1
+
+        angle = self.perception.joint["HeadYaw"]
+        temperature = self.perception.joint_temperature["HeadYaw"]
+
+
+        print 'HeadYaw angle: ' + str(angle) + ' temperature: ' + str(temperature)
 
         return action
 
